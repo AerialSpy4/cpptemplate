32ec66bd9a6f4e1d0a358cf8716440cd3e59f8ee
Spencer Yuan <aerialspy4@gmail.com>
2025-01-12 10:35:34 -0500

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,21 @@
+/*
+NOTES:
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int MM = 1e5+5;
+const int MOD = 1e9+7;
+
+void solve() {
+    
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T=1;
+    while (T--) {
+        solve();
+    }
+}
